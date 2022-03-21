9870435c1c347f55abcfd08df865281dd109ed6c
Bulciaa <julia.kalemba@protonmail.com>
2022-03-21 17:50:43 +0100

    Creating the `RankedGradeBook` class

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
 
+
 namespace GradeBook.GradeBooks
 {
-    public class StandardGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Standard;
+            Type = GradeBookType.Ranked;
         }
 
     }
